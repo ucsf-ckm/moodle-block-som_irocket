2fcec0e72ada8d9b72943208968b1d06ebe937f7
Carson Tam <carson.tam@ucsf.edu>
2012-06-22 17:35:02 -0700

    Initial checkin for SOM's iRocket block.

---
+++ README.txt
@@ -0,0 +1,25 @@
+Info
+======================
+Name: iROCKET Helpdesk
+Intro: The iROCKET Helpdesk blockis a Moodle blockthat provides iROCKET helpdesk contact information, and a link to the TOS.
+Version 1.0
+
+Authors
+======================
+Steven Williams ( williams@library.ucsf.edu )
+Company: UC San Francisco Center for Instructional Technology ( http://cit.ucsf.edu )
+Jesse Friedman  (jesse.friedman@ucsf.edu)
+Company: UC San Francisco SOM
+
+
+Features
+======================
+* Helpdesk Contact Information
+* Link to TOS
+    
+
+Installation
+======================
+1. Place som_irocket folder in blocks directory of local Moodle installation
+2. Log in to local Moodle instance as an administrator
+3. Select "Notifications" in Site Administration column
---
+++ block_som_irocket.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+
+ */
+
+  class block_som_irocket extends block_base {
+    function init() {
+      $this->title = 'iROCKET Helpdesk';
+      $this->version = 2009022607;
+  }
+   
+   function get_content() {
+   		global $CFG;
+   
+     if ($this->content !== NULL) {
+         return $this->content;
+     }
+ 
+     $this->content = new stdClass;
+     
+	 $logofile = $CFG->wwwroot.'/blocks/som_irocket/helpdesk-logo.gif';  
+     $somlogo = '<img src="'.$logofile.'" align="middle" alt="SOM iROCKET" title="SOM iROCKET">';
+    
+     
+     $this->content->text = $somlogo.
+     
+     '<br /><br />For assistance email the iROCKET Team at <a target="_blank" href="mailto:irocket@ucsf.edu">irocket@ucsf.edu</a> or call us at 415/502-2800. Our help desk is open M-F, 8a-5p.<br /><br /><a target="_blank" href="http://medschool.ucsf.edu/irocket/tech_requirement/policy.aspx#irocket_use">iROCKET Usage Policy </a>';
+     
+     
+ $this->content->footer ='';
+    
+  } 
+  
+}  
+ 
+ ?>
